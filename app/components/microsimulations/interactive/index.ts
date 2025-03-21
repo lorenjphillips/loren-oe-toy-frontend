adf8fc10cbf25da516bc7ae094dfed1a97d9e806
Loren Phillips <lphillips@zoox.com>
2025-03-20 22:45:34 -0700

    feat: interactive clinical simulation UI

--- app/components/microsimulations/interactive/index.ts
+++ app/components/microsimulations/interactive/index.ts
@@ -1,6 +1,6 @@
-export { default as ClinicalScenarioCard } from './ClinicalScenarioCard';
-export { default as TreatmentSelector } from './TreatmentSelector';
-export { default as DiagnosticTool } from './DiagnosticTool';
-export { default as OutcomeVisualizer } from './OutcomeVisualizer';
-export { default as TimelineProgress } from './TimelineProgress';
-export { default as SimulationLayout } from '../SimulationLayout'; 
+export { ClinicalScenarioCard } from './ClinicalScenarioCard';
+export { TreatmentSelector } from './TreatmentSelector';
+export { DiagnosticTool } from './DiagnosticTool';
+export { OutcomeVisualizer } from './OutcomeVisualizer';
+export { TimelineProgress } from './TimelineProgress';
+export { SimulationLayout } from '../SimulationLayout'; 
